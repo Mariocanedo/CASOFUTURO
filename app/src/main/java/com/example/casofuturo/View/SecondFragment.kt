2e466aa06042a3678824c7994e22bcaa8291c649
Mario canedo <Mario_cane.do@hotmail.com>
2024-05-30 11:23:04 -0400

    Proyecto terminado

--- app/src/main/java/com/example/casofuturo/View/SecondFragment.kt
+++ app/src/main/java/com/example/casofuturo/View/SecondFragment.kt
@@ -1,11 +1,14 @@
 package com.example.casofuturo.View
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -13,6 +16,7 @@ import com.bumptech.glide.Glide
 import com.example.casofuturo.R
 import com.example.casofuturo.ViewModel.CoursesViewModel
 import com.example.casofuturo.databinding.FragmentSecondBinding
+import kotlin.math.min
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -55,7 +59,8 @@ class SecondFragment : Fragment() {
 
         viewModel.getCourseDetail().observe(viewLifecycleOwner, Observer {
             Log.d("SELECCION3", courseId.toString())
-
+            var id = it.id
+            var name = it.title
             // cargamos datos desde la seleccion
 
             Glide.with(binding.ivLogo).load(it.image).into(binding.ivLogo)
@@ -67,6 +72,49 @@ class SecondFragment : Fragment() {
             binding.tvDuration.text = "Duracion del curso : ${it.weeks} "
             binding.tvStart.text ="Inicio de clases : ${it.start}"
 
+
+            // ACTION SEND EMAIL
+          binding.btMail.setOnClickListener {
+
+              Log.d("BUTTON", "Click")
+
+              val mintent = Intent(Intent.ACTION_SEND)
+
+              mintent.data = Uri.parse("mailto")
+              mintent.type = "text/plain"
+
+
+          // DIRECCION DE CORREO
+              mintent.putExtra(Intent.EXTRA_EMAIL, arrayOf("admisión@centrofuturo.cl"))
+
+              // ASUNTO
+              mintent.putExtra(Intent.EXTRA_SUBJECT,
+
+                  "SOLICITO INFORMACIÓN SOBRE ESTE CURSO" +id
+
+                  )
+
+              mintent.putExtra(
+
+                  Intent.EXTRA_TEXT, "HOLA\n" +
+
+                          "Quisiera pedir información sobre este curso " + name + " ,\n" +
+                          "me gustaría que me contactaran a este correo o al siguiente número\n" +
+                          " _________\n" +
+                          "Quedo atento."
+
+              )
+
+              try {
+
+                  startActivity(mintent)
+
+              } catch (e: Exception) {
+
+                  Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+              }
+
+          }
         })
 
 
